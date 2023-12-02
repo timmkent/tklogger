4beae236840f50abd78db276b970421e85516a73
Timm Kent <marcfelden@me.com>
2023-12-02 09:38:10 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/tklogger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tklogger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/logger.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class Logger {
+  static log(String message) {
+    // ignore: avoid_print
+    print("LOGGING:$message");
+  }
+}
